b5434fd4987cf359d128c3f9240f7af1bd6cd62a
Lorenzo Pagani <larrypg2@gmail.com>
2023-11-30 15:38:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script >
+import axios, { isCancel, AxiosError } from 'axios'
+import appHeader from './components/appHeader.vue'
+
+export default {
+  components: {
+    appHeader
+  },
+  data() {
+    return {
+      beerList: [],
+    }
+  },
+  mounted() {
+    axios.get("https://api.openbrewerydb.org/v1/breweries?by_country=ireland&per_page=10").then(resp => {
+      this.beerList.push(...resp.data)
+
+    })
+  }
+}
+</script>
+
+<template>
+  <appHeader title="Breweries of Ireland" />
+</template>
+
+<style scoped></style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    props: ["title"]
+}
+</script>
+<template>
+    <header class="d-flex justify-content-center p-3 bg-warning">
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+<script></script>
